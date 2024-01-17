84bf6acd1403f774117aa5b3196d946afebf8ba4
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-01-17 17:54:32 +0300

    Версия 0.1

--- DistanceCalc.cpp
+++ DistanceCalc.cpp
@@ -1,6 +1,38 @@
 ﻿#include <iostream>
+using namespace std;
 
+/// <summary>
+/// Метод расчёта пройденного пути
+/// </summary>
+/// <param name="V"></param>
+/// <param name="T"></param>
+/// <param name="A"></param>
+/// <returns>S = пройденный путь</returns>
+float DistanceCalc(float V, float T, float A) 
+{
+    float S = V * T + (A * T * T) / 2;
+    return S;
+}
 
-int main() {
-   
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+
+    // Переменные для хранения пользовательсого ввода
+    float V, T, A;
+
+    cout << "Введите скорость (V): ";
+    cin >> V;
+
+    cout << "Введите время (T): ";
+    cin >> T;
+
+    cout << "Введите ускорение (A): ";
+    cin >> A;
+
+    // Вычисляем пройденное расстояние по формуле
+    float S = DistanceCalc(V, T, A);
+
+    // Выводим результат
+    cout << "Пройденное расстояние: " << S << std::endl;  
 }
